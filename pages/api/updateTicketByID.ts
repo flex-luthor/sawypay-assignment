f3342a87923cba101d4634976f81b2e27e87ab6e
arpitFanClash <arpit@fanclash.in>
2023-04-11 04:04:36 +0530

    API error handling

--- pages/api/updateTicketByID.ts
+++ pages/api/updateTicketByID.ts
@@ -7,43 +7,44 @@ function validateStatusUpdate(nextStatus: string, prevStatus: string) {
   const diff = Math.abs(
     options.indexOf(nextStatus) - options.indexOf(prevStatus)
   );
-  console.log(diff);
 
   return diff <= 1;
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const mongoClient = await clientPromise;
-  const ticketID = req.body.ticketID;
-  const status = req.body.status;
-  console.log("next status", status);
+  try {
+    const mongoClient = await clientPromise;
+    const ticketID = req.body.ticketID;
+    const status = req.body.status;
 
-  console.log("ticketID", ticketID);
+    const ticket = await mongoClient
+      .db()
+      .collection("tickets")
+      .findOne({ ticketID: ticketID.toString() });
+    if (!ticket) return res.status(400).json({ error: "Invalid ID" });
 
-  const ticket = await mongoClient
-    .db()
-    .collection("tickets")
-    .findOne({ ticketID: ticketID.toString() });
-
-  if (!ticket) return res.status(400).json({ error: "Invalid ID" });
-  console.log("current status", ticket?.ticketStatus);
-
-  const validStatusUpdate = validateStatusUpdate(status, ticket?.ticketStatus);
-  if (!validStatusUpdate)
-    return res.status(400).json({ error: "Invalid Status" });
-
-  await mongoClient
-    .db()
-    .collection("tickets")
-    .updateOne(
-      { ticketID: ticketID },
-      { $set: { ticketID: ticketID, ticketStatus: status } }
+    const validStatusUpdate = validateStatusUpdate(
+      status,
+      ticket?.ticketStatus
     );
+    if (!validStatusUpdate)
+      return res.status(400).json({ error: "Invalid Status" });
 
-  const newTicket = await mongoClient
-    .db()
-    .collection("tickets")
-    .findOne({ ticketID: ticketID.toString() });
+    await mongoClient
+      .db()
+      .collection("tickets")
+      .updateOne(
+        { ticketID: ticketID },
+        { $set: { ticketID: ticketID, ticketStatus: status } }
+      );
 
-  res.status(200).json({ status: "UPDATED", data: newTicket });
+    const newTicket = await mongoClient
+      .db()
+      .collection("tickets")
+      .findOne({ ticketID: ticketID.toString() });
+
+    res.status(200).json({ status: "UPDATED", data: newTicket });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
 };
